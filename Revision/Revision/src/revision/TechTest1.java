39e169ec74646ce7fd6ff66aa605c482e2832e12
thamilton96 <55385321+thamilton96@users.noreply.github.com>
2019-10-03 14:22:00 +0100

    added new class for tech test

---
+++ Revision/Revision/src/revision/TechTest1.java
@@ -0,0 +1,12 @@
+package revision;
+/*Write a function called numberOfBracePairs that takes a string of braces,
+ and determines how many pairs of braces exist in that string.
+numberOfBracePairs should return the number of braces in the string or -1 if the input is not valid (more on valid/invalid inputs below)
+All input strings will be non null, and will only consist of open parentheses '(' , closed parentheses ')', open brackets '[', closed brackets ']',
+ open curly braces '{' and closed curly braces '}' */
+
+public class TechTest1 {
+	
+	
+
+}
